d63d60dd34ed0efcdf6aaab34c3e2c59afe662f6
andersberg <andersberg86@gmail.com>
2016-12-12 15:33:23 +0100

    Add basic server functionality

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+var express     = require('express');
+var server      = express();
+var port        = process.env.PORT || 4000;
+var bodyParser  = require('body-parser');
+var router      = express.Router();
+
+
+server.listen(port);
+console.log('The Butler Server is running on port: ' + port);
