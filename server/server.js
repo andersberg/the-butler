997ccb01a35a3ffad5958c64cf3367ab26b76c73
andersberg <andersberg86@gmail.com>
2016-12-13 10:50:57 +0100

    Update to use es6

--- server/server.js
+++ server/server.js
@@ -1,40 +1,42 @@
+
 // BASE SETUP
 // =============================================================================
-var express     = require('express');
-var server      = express();
-var port        = process.env.PORT || 4000;
-var bodyParser  = require('body-parser');
-var router      = express.Router();
-var apiRouter   = express.Router();
+import express, {Router} from 'express'
+import bodyParser from 'body-parser'
+
+const server      = new express()
+const port        = process.env.PORT || 8080
+const router      = new Router()
+const apiRouter   = new Router()
 
 // BODY PARSER SETUP
 // =============================================================================
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 // SERVER ROUTES
 // =============================================================================
 
 // Middleware
 router.use(function(req, res, next) {
-    console.log('Connection detected: ' + req.method + ' on ' + req.url);
-    next();
-});
+    console.log('Connection detected: ' + req.method + ' on ' + req.url)
+    next()
+})
 
 // Browser routes
 router.get('/', function(req, res) {
-    // res.send('<h1>Welcome to The Ape Butler Server!</h1>');
+    // res.send('<h1>Welcome to The Ape Butler Server!</h1>')
     
-});
-server.use('/', express.static('client'));
-// server.use('/', express.static(path.join(__dirname, 'client')));
+})
+server.use('/', express.static('client'))
+// server.use('/', express.static(path.join(__dirname, 'client')))
 
 // Api Routes
 apiRouter.get('/', function(req, res) {
-    res.send('<h1>Welcome to The Ape Butler API!</h1>');
+    res.send('<h1>Welcome to The Ape Butler API!</h1>')
 })
 
-server.use('/api', apiRouter);
+server.use('/api', apiRouter)
 
-server.listen(port);
-console.log('The Butler Server is running on port: ' + port);
+server.listen(port)
+console.log('The Butler Server is running on port: ' + port)
