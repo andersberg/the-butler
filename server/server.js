e158a76d456e536ad7348cd8e8762e613460b01a
andersberg <andersberg86@gmail.com>
2016-12-14 09:57:45 +0100

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
+'use strict'
 
 // BASE SETUP
 // =============================================================================
 import express, {Router} from 'express'
+import http from 'http'
 import bodyParser from 'body-parser'
+import socketIo from 'socket.io'
 
-const server      = new express()
-const port        = process.env.PORT || 8080
-const router      = new Router()
-const apiRouter   = new Router()
+const app           = new express()
+const server        = http.Server(app)
+const port          = process.env.PORT || 8080
+const router        = new Router()
+const apiRouter     = new Router()
+const io            = new socketIo(server)
+
 
 // BODY PARSER SETUP
 // =============================================================================
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // SERVER ROUTES
 // =============================================================================
@@ -28,8 +34,13 @@ router.get('/', function(request, response) {
     // response.send('<h1>Welcome to The Ape Butler Server!</h1>')
     
 })
-server.use('/', express.static('client'))
-// server.use('/', express.static(path.join(__dirname, 'client')))
+app.use('/', express.static('client'))
+// app.use('/', express.static(path.join(__dirname, 'client')))
+
+io.on(`connection`, (socket) => {
+    console.log(`Connection detected!`)
+})
+
 
 // Api Routes
 apiRouter.get('/', function(request, response) {
@@ -41,7 +52,10 @@ apiRouter.post(`/`, (request, response) => {
     response.json({ message: `Welcome to The Butler API`})
 })
 
-server.use('/api', apiRouter)
+app.use('/api', apiRouter)
 
-server.listen(port)
+app.listen(port)
+// server.listen(8080,() => {
+//     console.log(`listening on 8080`)
+// })
 console.log('The Butler Server is running on port: ' + port)
